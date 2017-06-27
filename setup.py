26d4458798fb9de04b7f892cbad7e624ec227fe5
David GEISSBUHLER <dgeissbuhler@italix29.idiap.ch>
2017-06-27 16:08:48 +0200

    Added original C++ code by Robby Tan for specular highlights removal with corresponding python bindings.'

--- setup.py
+++ setup.py
@@ -3,11 +3,18 @@
 # Sushil Bhattacharjee <sushil.bhattacharjee@idiap.ch>
 # Tue 7 Mar 2017 11:26:26 CET
 
-from setuptools import setup, find_packages, dist
-dist.Distribution(dict(setup_requires=['bob.extension']))
+setup_packages = ['bob.extension', 'bob.blitz']
+bob_packages = []
 
+from setuptools import setup, find_packages, dist
+dist.Distribution(dict(setup_requires = setup_packages + bob_packages))
+
+# import the Extension class and the build_ext function from bob.blitz
+from bob.blitz.extension import Extension, build_ext
+
+# load the requirements.txt for additional requirements
 from bob.extension.utils import load_requirements
-requirements = load_requirements()
+requirements = setup_packages + bob_packages + load_requirements()
 
 version = open("version.txt").read().rstrip()
 
@@ -30,6 +37,42 @@ setup(
     include_package_data=True,
 
     install_requires = requirements,
+
+    # We are defining two extensions here. Each of them will be compiled
+    # independently into a separate .so file.
+    ext_modules = [
+
+      # The first extension defines the version of this package and all C++-dependencies.
+      Extension("bob.ip.qualitymeasure.version",
+        # list of files compiled into this extension
+        [
+          "bob/ip/qualitymeasure/version.cpp",
+        ],
+        # additional parameters, see Extension documentation
+        version = version,
+        bob_packages = bob_packages,
+      ),
+
+      # The second extension contains the actual C++ code and the Python bindings
+      Extension("bob.ip.qualitymeasure._library",
+        # list of files compiled into this extension
+        [
+          # the pure C++ code
+          "bob/ip/qualitymeasure/tan_specular_highlights_orig.cpp",
+          # the Python bindings
+          "bob/ip/qualitymeasure/main.cpp",
+        ],
+        # additional parameters, see Extension documentation
+        version = version,
+        bob_packages = bob_packages,
+      ),
+    ],
+
+    # Important! We need to tell setuptools that we want the extension to be
+    # compiled with our build_ext function!
+    cmdclass = {
+      'build_ext': build_ext,
+    },
 
     entry_points={
       # scripts should be declared using this entry:
